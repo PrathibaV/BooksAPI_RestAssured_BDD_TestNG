0d2d0ce37c4228e7f049be4ea395fedd370a94c0
PrathibaV <prathibavenkatesan89@gmail.com>
2025-05-04 08:14:52 -0500

    sequential run

--- src/test/java/com/books/runner/TestRunner.java
+++ src/test/java/com/books/runner/TestRunner.java
@@ -1,17 +1,22 @@
 package com.books.runner;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
+import org.testng.annotations.DataProvider;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.PickleWrapper;
 
-@CucumberOptions(features = "src/test/resources/features", 
-		glue = {"com.books.stepdefinitions",  "com.books.hooks"},             
-	    plugin = {
-	        "pretty",                                      
-	        "html:target/cucumber-reports.html",            
-	    },
-	    monochrome = true                                // Clean console output
-	    //tags = "@SmokeTest"                                
-	)
+@CucumberOptions(features = "src/test/resources/features", glue = { "com.books.stepdefinitions",
+				"com.books.hooks" }, plugin = { "pretty", "html:target/cucumber-reports.html", }, monochrome = true // Clean
+																													// console
+																													// output
+// tags = "@SmokeTest"
+)
 public class TestRunner extends AbstractTestNGCucumberTests {
+
+	
 
 }
