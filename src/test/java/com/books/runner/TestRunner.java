e8c9ae98acd3d8d6c63aa144d900aa5738c3a33e
PrathibaV <prathibavenkatesan89@gmail.com>
2025-05-12 16:15:28 -0500

    integrated chainTest reporting

--- src/test/java/com/books/runner/TestRunner.java
+++ src/test/java/com/books/runner/TestRunner.java
@@ -6,8 +6,15 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features = "src/test/resources/features", glue = { "com.books.stepdefinitions",
-				"com.books.hooks" }, plugin = { "pretty", "html:target/cucumber-reports.html", }, monochrome = true
+@CucumberOptions(
+		features = "src/test/resources/features",
+		glue = { "com.books.stepdefinitions", "com.books.hooks" }, 
+		plugin = { 
+				"pretty", 
+				"html:target/cucumber-reports.html", 
+				"com.aventstack.chaintest.plugins.ChainTestCucumberListener:"}, 
+		dryRun = false,
+		monochrome = true
 				//, tags = "@wip"
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
